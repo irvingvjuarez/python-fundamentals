ac416b3c912ef03907fd131f6101c87f0c08e825
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-28 18:30:04 -0600

    Returning country code in main.py

--- fetching/main.py
+++ fetching/main.py
@@ -1,10 +1,13 @@
 import requests
 
-URL_API = "https://api.nationalize.io?name=michael"
+URL_API = lambda name: f"https://api.nationalize.io?name={name}"
 
-def main():
-    r = requests.get(URL_API).json()
-    print(r)
+def get_country_code(name):
+    res = requests.get(URL_API(name)).json()
+    country_code = res["country"][0]["country_id"]
+
+    return country_code
 
 if __name__ == "__main__":
-    main()
+    country_code = get_country_code("Irving")
+    print(country_code)
