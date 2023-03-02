b62821f6d7a983ab873d553988aaff052f45eadb
Irving Juárez <irvingjuarez274@gmail.com>
2023-03-02 13:39:38 -0600

    Using the execution time decorator to know the time of each function

--- sets/main.py
+++ sets/main.py
@@ -1,15 +1,24 @@
+import sys
+sys.path.append("../decorators")
+
+from execution_time import execution_time
+
+@execution_time
 def remove_duplicates(lst):
     return list( set(lst) )
 
+@execution_time
 def remove_duplicates_w_for(lst):
-    to_set = set({})
+    non_repeated = []
+
     for item in lst:
-        to_set.add(item)
-        
-    return list(to_set)
+        if item not in non_repeated:
+            non_repeated.append(item)
+
+    return non_repeated
 
 if __name__ == "__main__":
-    my_list = [7,3,7,8,3,6,0,9,4,9,4,3,7,3,1]
+    my_list = [7,3,7,8,3,6,0,9,4,9,4,3,7,3,1,7,8,2,7,3,6,3,6,7,2,7,6,2,7,6,1,2,7,2,1,1,9,1,9,2,0,0,4,3,8]
     print(
         remove_duplicates(my_list)
     )
